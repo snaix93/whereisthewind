edc64565ecdebff7ba87e2d5885424c123fd33f2
Alex <snaix93@gmail.com>
2024-05-23 12:44:36 +0100

    Version 1.1 - Updated ReadMe

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -27,7 +27,6 @@ class AuthenticationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
